7d910a0413da62614e1838c30b3b7270679dc4a2
Foehn <Foehn@DESKTOP-U0FITF0>
2023-07-27 23:30:16 +0800

    2023年7月27日 23点30分

--- MySQL_Display/Form1.cs
+++ MySQL_Display/Form1.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+
 
 namespace MySQL_Display
 {
@@ -15,6 +19,7 @@ namespace MySQL_Display
     {
 
         MySqlConnection connection = new MySqlConnection("Server=localhost;User ID=root;Password=123321");
+        List<string> choose_item = new List<string>();
 
         // 主函数调用空参构造器，以初始化窗口
         public Form1()
@@ -25,36 +30,35 @@ namespace MySQL_Display
         
         private void MainForm_Load(object sender, EventArgs e)
         {
+            listBox1.Items.Clear();
             try
             {
                 connection.Open();
+                var getRow = new MySqlCommand("SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = 'fabric' AND TABLE_NAME = 'fabic_color';", connection);
+
+                var Rowname = getRow.ExecuteReader();
+
+                //List<string> row_name_list = new List<String>();
+
+                while (Rowname.Read())
+                {
+                    listBox1.Items.Add(Rowname.GetString(0));
+                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("代码中的连接数据库的参数可能不对！");
+                MessageBox.Show("代码中的连接数据库的参数可能不对！"); 
             }
             finally
             {
                 connection.Close();
-            }
-
-            var getRow = new MySqlCommand("SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = 'fabic' AND TABLE_NAME = 'fabic_color';",connection);
-
-            var Rowname = getRow.ExecuteReader();
-
-            //List<string> row_name_list = new List<String>();
-
-            while (Rowname.Read())
-            {
-                listBox1.Items.Add(Rowname.GetString(0));
-            }
+            }            
         }
 
         private void listBox1_SelectedValueChanged(object sender, EventArgs e)
         {
-            
+            choose_item.Clear();
             // 所选项遍历进集合choose_item
-            List<string> choose_item = new List<string>();
             foreach (var item in listBox1.SelectedItems)
             {
                 choose_item.Add(item.ToString());
@@ -66,30 +70,51 @@ namespace MySQL_Display
             try
             {
                 connection.Open();
+
+                // 获取所选波长范围
+                String wave_min = textBox1.Text;
+                String wave_max = textBox2.Text;
+
+                // 拼接字符串
+                StringBuilder col_name = new StringBuilder();
+                foreach (var item in choose_item)
+                {
+                    string tmp_col = "`"+item.ToString()+"`";
+
+                    if (col_name.Length == 0) 
+                    { 
+                        col_name.Append(tmp_col);
+                    }
+                    else
+                    {
+                        col_name.Append(","+tmp_col);
+                    }
+
+                }
+
+                // 拼接SQL
+                String commmand_string = "Select "+col_name.ToString()+ " from fabric.fabic_color where fact_wave between "+wave_min+" and "+wave_max+";";
+
+                var getRow = new MySqlCommand(commmand_string, connection);
+
+                var query_result = getRow.ExecuteReader();
+
+                DataTable dataTable = new DataTable();
+                query_result.Read();
+                dataTable.Load(query_result);
+                dataGridView1 = new DataGridView();
+                dataGridView1.DataSource = dataTable;
+                
             }
             catch (Exception ex)
             {
-                MessageBox.Show("代码中的连接数据库的参数可能不对！");
+                MessageBox.Show("代码中的拼接SQL可能不对！"); 
             }
-            finally
-            {
-                connection.Close();
+            finally 
+            { 
+                connection.Close(); 
             }
 
-            // 获取所选波长范围
-            String wave_min = textBox1.Text;
-            String wave_max = textBox2.Text;
-
-            string commmand_string =;
-            var getRow = new MySqlCommand("SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = 'fabic' AND TABLE_NAME = 'fabic_color';", connection);
-
-            var Rowname = getRow.ExecuteReader();
-
-
-            while (Rowname.Read())
-            {
-                listBox1.Items.Add(Rowname.GetString(0));
-            }
         }
     }
 }
