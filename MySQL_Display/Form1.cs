ce54e8755f3f051478dd122defcdfcfd349c7a4d
枫 邓 <offened@foxmail.com>
2023-07-27 13:52:49 +0800

    项目新建

---
+++ MySQL_Display/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MySQL_Display
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection _connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
